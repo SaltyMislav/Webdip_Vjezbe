589494918c1c04368e1870d2f4d919b2d257b413
Mislav <znidarec01@gmail.com>
2023-06-09 17:50:38 +0200

    drugi dio

--- javascript/mznidarec.js
+++ javascript/mznidarec.js
@@ -1,5 +1,16 @@
 window.onload = function () {
-  const input1 = document.querySelector(".prvi_element");
+  let naslov = document.title;
+  switch (naslov) {
+    case "Obrazac":
+      popunjavanjeObrasca();
+      postavljenjeGumbaDalje();
+      break;
+  }
+};
+
+let brojPolja = 1;
+
+function popunjavanjeObrasca() {
   const input2 = document.querySelector(".drugi_element");
   const input3 = document.querySelector(".treci_element");
   const input4 = document.querySelector(".cetverti_element");
@@ -7,14 +18,43 @@ window.onload = function () {
   const input6 = document.querySelector(".sesti_element");
   const inputDodatniElementi = document.querySelector(".drugi");
 
-  const dalje = document.getElementsByClassName("dalje");
-
-  console.log(input2);
-
   input2.style.visibility = "hidden";
   input3.style.visibility = "hidden";
   input4.style.visibility = "hidden";
   input5.style.visibility = "hidden";
   input6.style.visibility = "hidden";
-  inputDodatniElementi.style.display = "none";
-};
+  inputDodatniElementi.style.visibility = "hidden";
+}
+
+function postavljenjeGumbaDalje() {
+  console.log("tui sam");
+  document
+    .querySelector(".dalje")
+    .addEventListener("click", () => provjeraPolja());
+}
+
+function provjeraPolja() {
+  switch (brojPolja) {
+    case 1:
+      provjeraNaslova();
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    case 6:
+      break;
+    case 7:
+      document.querySelector(".dalje").disabled = true;
+      break;
+    default:
+      document
+        .querySelector(".dalje")
+        .removeEventListener("click", () => provjeraPolja());
+  }
+  brojPolja++;
+}
